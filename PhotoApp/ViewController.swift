a933d6e5735e28223112f48650a231a896be8ab5
Ignacio Arias <1992ignacio@gmail.com>
2021-01-20 19:37:30 -0800

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Ignacio Arias on 2021-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
